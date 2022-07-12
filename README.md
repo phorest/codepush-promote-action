024fa7f841b06e4ef3606015c800e327881b0175
Nicola Zaghini <nicola.zaghini@phorest.com>
2022-07-12 14:02:40 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# codepush-promote
-This GH Action promotes in MS CodePush with Options
+# CodePush Promote Action
+
+This action promotes latest CodePush label from an ENV to another.
+
+## Inputs
+
+## `mandatory`
+
+A boolean indicating if the version is mandatory or not. Default `"false"`.
+
+## `rollout`
+
+A string indicating the percentage of the rollout. Default `"100"`.
+
+## `from`
+
+A string indicating the `"from"` CodePush env, i.e. `"DEV"` 
+
+## `to`
+
+A string indicating the `"to"` CodePush env, i.e. `"PROD"`
+
+## `iOSAppName`
+
+A string indicating the CodePush iOS app name
+
+## `androidAppName`
+
+A string indicating the CodePush Android app name
+
+## Outputs
+
+## `releaseTag`
+
+The tag of the release
+
+
+## `releaseName`
+
+The name of the release
+
+## `releaseUrl`
+
+The url of the release
+
+
+## Example usage
+
+    uses: actions/cp-promote-action@v1.0
+    with:
+        mandatory: 'false'
+        rollout: '100'
+        from: 'STAG'
+        to: 'PROD'
+        iOSAppName: 'PhorestGo-iOS'
+        androidAppName: 'PhorestGo-Android'
+    env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        APPCENTER_TOKEN: ${{ secrets.APPCENTER_TOKEN }}
---
+++ index.js
@@ -0,0 +1,75 @@
+const core = require('@actions/core');
+const { GitHub, context } = require('@actions/github');
+const axios = require("axios");
+
+async function run() {
+  try {
+    // Get inputs
+    const mandatory = core.getInput('mandatory');
+    const rollout = core.getInput('rollout');
+    const from = core.getInput('from');
+    const to = core.getInput('to');
+    const iOSAppName = core.getInput('iOSAppName');
+    const androidAppName = core.getInput('androidAppName');
+
+    // Make network requests
+    const iosResult = await promote(iOSAppName, mandatory, rollout, from, to)
+    const androidResult = await promote(androidAppName, mandatory, rollout, from, to)
+
+    // Extract info from responses
+    const binary = iosResult.data.target_binary_range.replace(/[<>~]+/,'')
+    const label = iosResult.data.label
+    const originalLabel = iosResult.data.original_label
+
+    // New Variables
+    const tagName = `PRODUCTION-${binary}-${label}(${originalLabel})`
+    const releaseName = `${binary}${label}`
+
+    // Release and Tag
+    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const release = await github.repos.createRelease({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      tag_name: tagName,
+      target_commitish: context.sha,
+      name: releaseName,
+      body: releaseName,
+      draft: 'false',
+      prerelease: 'false'
+    });
+
+    core.setOutput('releaseName', releaseName);
+    core.setOutput('releaseTag', tagName);
+    core.setOutput('releaseUrl', release.data.html_url);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+function promote(appName, mandatory, rollout, from, to) {
+  const url = `https://api.appcenter.ms/v0.1/apps/Phorest/${appName}/deployments/${from}/promote_release/${to}`
+  const data = {
+    isMandatory: mandatory,
+    rollout: rollout
+  }
+  const headers = {
+    "X-API-Token": process.env.APPCENTER_TOKEN,
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+  }
+  return axios.post(url, data,{headers})
+}
+
+run()
+
+
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
